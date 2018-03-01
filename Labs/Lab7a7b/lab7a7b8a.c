a22309b4059afbc927e90cbd0b09ac1e3e430510
gadriwau <gadriwau@mcmaster.ca>
2018-03-01 11:00:16 -0700

    Lab7a7b8a done

--- Labs/Lab7a7b/lab7a7b8a.c
+++ Labs/Lab7a7b/lab7a7b8a.c
@@ -4,7 +4,7 @@
 
 #define BUFFER_SIZE 10
 
-#define OFFSET_MASK ‭4095
+#define OFFSET_MASK 4095
 #define PAGES 8
 #define OFFSET_BITS 12
 #define PAGE_SIZE 4096
@@ -21,13 +21,17 @@ int main(void) {
 	unsigned int logicalAddress;
 	unsigned int pageNumber;
 	int offset;
+	int frameNumber;
 	unsigned int physicalAddress;
+	int page_table[PAGES] = {6,4,3,7,0,1,2,5};
 	while (fgets(buff, BUFFER_SIZE, fptr) != NULL) {
 		// buff is a pointer to each line/address from the file
 		logicalAddress = atoi(buff);
 		pageNumber = logicalAddress >> OFFSET_BITS;
 		offset = logicalAddress & 4095;
-		printf("Virtual addr is %d: Page# = %d & Offset = %d. Physical addr = %d\n",
+		frameNumber = page_table[pageNumber];
+		physicalAddress = (frameNumber << OFFSET_BITS) | offset;
+		printf("Virtual addr is %d: Page# = %d & Offset = %d. Physical addr = %d.\n",
 		logicalAddress, pageNumber, offset, physicalAddress);
 	}
 	
