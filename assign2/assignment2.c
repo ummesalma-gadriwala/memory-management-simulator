ebda12d0c5a8cb9fe6282a1ca46622502b7599ce
gadriwau <gadriwau@mcmaster.ca>
2018-03-05 09:19:35 -0700

    set up assignment 2, implemented minor functions

---
+++ assign2/assignment2.c
@@ -0,0 +1,71 @@
+// Tasnim Noshin
+// Umme Salma Gadriwala
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 10
+
+#define OFFSET_MASK 2047
+#define PAGES 32
+#define OFFSET_BITS 11
+#define PAGE_SIZE 2048
+/**
+Page size = 256 bytes = 2048 bits = 2^11
+Number of bits required to represent offset = 11
+Number of bits required to represent page number = 5
+Number of pages = 2^5
+Size of LAS = 2^16
+Size of PAS = 2^15
+**/
+struct TLBentry {
+	int pageNumber;
+	int frameNumber;
+};
+
+/* return frameNumber corresponding to pageNumber */
+int search_TLB(int pageNumber) {
+	return -1;
+}
+
+/* return 0 if success, -1 if error occurs */
+int TLB_Add(void) {
+	return -1;
+}
+
+/* return 0 if success, -1 if error occurs */
+int TLB_Update(void) {
+	return -1;
+}
+
+int main(void) {
+	FILE *fptr = fopen("addresses.txt", "r");
+	char buff[BUFFER_SIZE];
+	unsigned int logicalAddress;
+	int physicalAddress;
+	int hitCount = 0; // total number of TLB hits
+	int faultCount = 0; // total number of page-faults
+	unsigned int pageNumber;
+	int offset;
+	int frameNumber;
+	int page_table[PAGES];
+	int i;
+	for (i = 0; i < PAGES; i++) {
+		page_table[i] = -1;
+	}
+	while (fgets(buff, BUFFER_SIZE, fptr) != NULL) {
+		// buff is a pointer to each line/address from the file
+		logicalAddress = atoi(buff);
+		pageNumber = logicalAddress >> OFFSET_BITS;
+		offset = logicalAddress & OFFSET_MASK;		
+		// LOOK UP TLB
+		// LOOK UP PAGE TABLE
+		frameNumber = page_table[pageNumber];
+		physicalAddress = (frameNumber << OFFSET_BITS) | offset;
+		printf("Virtual address: %d - Page# = %d & Offset = %d. Physical address: %d\n",
+		logicalAddress, pageNumber, offset, physicalAddress);
+	}
+	
+	fclose(fptr);
+	return 0;
+}
