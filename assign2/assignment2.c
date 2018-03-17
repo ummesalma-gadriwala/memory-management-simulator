55e0672018bde650be69f730d0272a7fb5dfa22e
gadriwau <gadriwau@mcmaster.ca>
2018-03-16 21:03:56 -0600

    54 damn hits

--- assign2/assignment2.c
+++ assign2/assignment2.c
@@ -50,13 +50,6 @@ int search_TLB(int page) {
 }
 
 // return new position of the oldest entry in TLB
-//int TLB_Add(TLBentry *newEntry, int oldestEntryPosition) {
-//	TLBarray[oldestEntryPosition%TLB_SIZE] = newEntry;
-//	oldestEntryPosition++;
-//	return oldestEntryPosition;
-//}
-
-// return new position of the oldest entry in TLB
 // update page and frame numbers at position
 int TLB_Add(int page, int frame, int oldestEntryPosition) {
 	TLBarray[oldestEntryPosition%TLB_SIZE]->pageNumber = page;
@@ -140,30 +133,27 @@ int main(void) {
 				nextTLBFrame = TLB_Add(pageNumber, frameNumber, nextTLBFrame);
 				// when page fault occurs
 			} else {
-				//if (page_table[pageNumber]==-1){
-					faultCount++;
+				faultCount++;
 				// get the page at the pageNumber from the mapped data
 				// put it in the next available frame
 				// change the page table entry
 				// update page table
-					int oldPage;
-					memcpy(physical_memory[nextFrame%FRAMES], mmapfptr + (pageNumber*256), 256);
-					for (i = 0; i < PAGES; i++) {
-						if (page_table[i] == nextFrame%FRAMES) {
-							page_table[i] = -1;
-							oldPage = i;
-						}
+				int oldPage = -1;
+				memcpy(physical_memory[nextFrame%FRAMES], mmapfptr + (pageNumber*256), 256);
+				for (i = 0; i < PAGES; i++) {
+					if (page_table[i] == nextFrame%FRAMES) {
+						page_table[i] = -1;
+						oldPage = i;
 					}
-					page_table[pageNumber] = nextFrame%FRAMES;
-					frameNumber = page_table[pageNumber];
-					nextFrame++;
-					int success = TLB_Update(oldPage, pageNumber);
-					if (success == -1){
-						nextTLBFrame = TLB_Add(pageNumber, frameNumber, nextTLBFrame);
-					}
-					frameNumber = page_table[pageNumber];				
 				}
-			//}
+				page_table[pageNumber] = nextFrame%FRAMES;
+				frameNumber = page_table[pageNumber];
+				nextFrame++;
+				int success = TLB_Update(oldPage, pageNumber);
+				if (success == -1) {
+					nextTLBFrame = TLB_Add(pageNumber, frameNumber, nextTLBFrame);
+				}
+			}
 		}
 		physicalAddress = (frameNumber << OFFSET_BITS) | offset;
 		value = physical_memory[frameNumber][offset];
@@ -183,6 +173,6 @@ int main(void) {
 	for(i = 0; i < TLB_SIZE; i++) {
 		free(TLBarray[i]);
 	}
-	/////////////////////////////////////////////////////
+	//////////////////////////////////////////////
 	return 0;
 }
--- assign2/q2.py
+++ assign2/q2.py
@@ -1,20 +1,45 @@
-#import os
 def main():
-    length = 0
-    lenLater = 0
-    inputFile = open('output-corrected.txt','r')
+    #length = 0
+    #lenLater = 0
+    inputFile = open('output.txt','r')
     result = open('result.txt','r')
-    #print(inputFile.readline())
+
     lineRead = inputFile.readline()
-    for i in range(1000):
-    #while (lineRead != ' '):
+
+    for i in result.readlines():
+    	if i[0] in ('T', 'P'):
+    		break
+	r = i.split(' ')
+	o = lineRead.split(' ')
+	
+	rVA = r[2]
+	rPN = r[3][6:]
+	rO = r[4][7:]
+	rPA = r[8]
+	rV = r[9][6:]
+	
+	oVA = o[2]
+	oPN = o[3][6:]
+	oO = o[4][7:]
+	oPA = o[8]
+	oV = o[9][6:]
+	
+	if not ((rVA == oVA) and (rPN == oPN) and (rO == oO) and (rPA == oPA) and (rV == oV)):
+		print ("Result", rVA, rPN, rO, rPA, rV)
+		print ("Output", oVA, oPN, oO, oPA, oV)
+	
+	lineRead = inputFile.readline()
+	return
+
+'''
+    	lineRead = i
         length = length +1
             
         lineInput = lineRead
         lineInput = lineInput.split(' ')
         lineResult = result.readline()
         lineResult = lineResult.split(' ')
-        # print(lineInput)
+        #print(lineInput)
         #print(lineResult)
         virtualMemory = lineInput[2]
         virtualMemoryResult = lineResult[2]
@@ -41,5 +66,6 @@ def main():
     print(lenLater)
     inputFile.close()
     result.close()
+'''
 main()
     
