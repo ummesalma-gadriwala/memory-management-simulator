8457381f0bb6b3a3b3d7e6c5ceec0419ee4dcd86
UMME SALMA Gadriwala <umme.salma.gadriwala@ibm.com>
2018-03-19 18:03:55 -0400

    test.py

--- mmu/test.py
+++ mmu/test.py
@@ -30,42 +30,5 @@ def main():
 	
 	lineRead = inputFile.readline()
 	return
-
-'''
-    	lineRead = i
-        length = length +1
-            
-        lineInput = lineRead
-        lineInput = lineInput.split(' ')
-        lineResult = result.readline()
-        lineResult = lineResult.split(' ')
-        #print(lineInput)
-        #print(lineResult)
-        virtualMemory = lineInput[2]
-        virtualMemoryResult = lineResult[2]
-        phyMem = lineInput[6]
-        phyMemResult = lineResult[8]
-        valIn = lineInput[7]
-        valRes = lineResult[9]
-        #print(valRes)
-        #valRes = valRes[0:8]
-        # print("V: ",virtualMemory, " R: ",virtualMemoryResult)
-        # print("V: ",phyMem, " R: ",phyMemResult)
-        #print("V: ",valIn, " R: ",valRes)
-        if(virtualMemory == virtualMemoryResult):
-            if(phyMem == phyMemResult):
-                    if(valIn == valRes):
-                        lenLater = lenLater + 1
-                    else:
-                        print(valIn, " ", valRes)
-                       
-        lineRead = inputFile.readline()
-         
-    #in result get rid of \n at the end
-    print(length)
-    print(lenLater)
-    inputFile.close()
-    result.close()
-'''
 main()
     
